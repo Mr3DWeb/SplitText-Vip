c5b8acbe2f6e0d361b722e1fd02686aab91092b8
Mohammad Sharyatzadeh <mr3dweb@gmail.com>
2025-12-30 23:45:05 +0330

    v1 Done

--- src/script.js
+++ src/script.js
@@ -18,11 +18,42 @@ function HoverSpAnimation(btnHover,hoverAnimation={}){
       hoverTL.restart();
     }
   })
-
 }
+
+/**
+ * @param {string} contentWarpper
+ * @param {gsap.TweenVars} lineAnimation
+ */
+function LineSpAnimation(contentWarpper,lineAnimation){
+  const warpper = document.querySelector(contentWarpper);
+
+  const paragraf = warpper.querySelector('p');
+  const play = warpper.querySelector('.play');
+  const stop = warpper.querySelector('.stop');
+  const reverse = warpper.querySelector('.reverse');
+
+  const spLine = new SplitText(paragraf,{type:"lines",tag:"p"});
+  const lineTL = gsap.timeline({paused:true , defaults:{immediateRender: false}});
+  lineTL.from(spLine.lines,lineAnimation);
+
+  play.addEventListener('click',()=> {
+    if(!lineTL.isActive()){
+      lineTL.restart();
+    }
+  });
+  stop.addEventListener('click',()=> lineTL.pause());
+  reverse.addEventListener('click',()=> lineTL.reverse());
+}
+
 
 window.addEventListener('load',()=>{
   // Enter Animation
+  const enterTL = gsap.timeline({defaults:{direction:2,opacity:0}});
+  enterTL
+        .from("section div",{stagger:0.1})
+        .from(".counter",{rotationY:360,transformOrigin:"50% 50%",stagger:0.4,scale:3})
+        .from("footer",{y:30 , ease:"back"})
+
   // Hover Animation 1
   HoverSpAnimation('#Hover-btn1',{
     opacity:0,
@@ -52,27 +83,34 @@ window.addEventListener('load',()=>{
     duration:1,
     transformOrigin:"0% 100%"
   });
+
+  //Line Animation 1 
+  LineSpAnimation('#lineAnimation1',{
+  opacity:0,
+  stagger:0.1, 
+  rotationX:-90,
+  rotationY:45,
+  duration:1.5,
+  transformOrigin:"50& 50% -150&"
+  });
+  //Line Animation 2 
+  LineSpAnimation('#lineAnimation2',{
+  opacity:0,
+  stagger:0.1, 
+  duration:1,
+  y:80,
+  scale:0.6
+  });
+  //Line Animation 3 
+  LineSpAnimation('#lineAnimation3',{
+  opacity:0,
+  y:100,
+  skewY:7,
+  filter:"blur(10px)",
+  transformOrigin:"0% 100%",
+  stagger:0.1,
+  duration:0.8,
+  ease:"power4.out"
+  });
+
 })
-
-
-
-
-// const splitWords = new SplitText("h2",{type:"words"})
-
-// const spWords = gsap.from(splitWords.words,{
-//   opacity:0,
-//   stagger:0.2,
-//   ease:"back(4)",
-//   y:50,
-//   rotation:-40
-// })
-
-// const splitLine = new SplitText("p",{type:"lines"})
-// const spLine = gsap.from(splitLine.lines,{
-//   opacity:0,
-//   stagger:0.5, 
-//   rotationX:-90,
-//   rotationY:45,
-//   duration:1.5,
-//   transformOrigin:"50& 50% -150&"
-// })
